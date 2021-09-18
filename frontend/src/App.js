2fffd68e17e7a2544936a6307023bc537e75e9c0
vishalda <vdavishu@gmail.com>
2021-09-18 23:59:20 +0530

    Added Home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Missing from "./components/missing/missing";
 import Register from "./components/register/register";
-import Navbar from "./components/navbar/navbar";
 import { Route, BrowserRouter as Router } from "react-router-dom";
+import Home from "./components/home/home";
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <Route path="/" component={Home} />
       <Route path="/register" component={Register} />
       {/* map route */}
       {/* <Route path="/map" component={Map} /> */}
