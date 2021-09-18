c59e5b7644f22d39921165371f157b386acb4e65
rutuja <dhanawaderutuja1002@gmail.com>
2021-09-18 14:23:55 +0530

    added user model

--- api/search/views.py
+++ api/search/views.py
@@ -10,18 +10,20 @@ from .serializers import MissingPersonListSerializer
 # Create your views here.
 @csrf_exempt
 def addMissingPerson(request):
-    if request.method!="POST":
-        return JsonResponse({'error':'Accepting only Post request'})
-    
+    if request.method != "POST":
+        return JsonResponse({'error': 'Accepting only Post request'})
+
     name = request.POST['name']
     description = request.POST['description']
     photo = request.FILES['photo']
     age = request.POST['age']
     place = request.POST['place']
     found = False
-    instance = MissingPersonModal.objects.create(name=name,description=description,photo=photo,age=age,place=place,found=found)
+    instance = MissingPersonModal.objects.create(name=name, description=description, photo=photo, age=age, place=place,
+                                                 found=found)
     instance.save()
-    return JsonResponse({'success':'Added person successfully'})
+    return JsonResponse({'success': 'Added person successfully'})
+
 
 class PersonViewSet(viewsets.ModelViewSet):
     queryset = MissingPersonModal.objects.all().order_by('id')
